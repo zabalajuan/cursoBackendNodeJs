444f6503160c67d6197f10a2a1cfea14f97edc5c
Juan Zabala <zabala1juan@gmail.com>
2023-12-16 17:42:51 -0500

    Clase 11 - CRUD con sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -12,7 +12,8 @@ class UserService {
   }
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
@@ -28,17 +29,23 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if(!user){
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    // const user = await models.User.findByPk(id);
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
