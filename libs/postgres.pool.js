55c9eb073aef25ce18706f2cad136f404365386c
Juan Zabala <zabala1juan@gmail.com>
2023-12-16 13:41:29 -0500

    Clase 8 - Variables de ambiente

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -4,15 +4,28 @@ const {Pool} = require('pg');
 //pg internamente con la primera conexion genera un await, por lo cual ya no es necesario que lo manejemos asi
 //una vez genera su await interno, reparte las conexiones cuando se requiere.
 
-const pool = new Pool({
+//Importamos las credenciales
+const {config} = require('./../config/config');
+
+//una buena practica es mandar una url con todo el esquema de conexion
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+//Crearemos la URL de conexion
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const pool = new Pool({connectionString: URI});
   //configuración de la conexion
   //como lo estamos corriendo con docker, le ponemos local
-  host:'localhost',
-  port:5432, //donde esta corriendo la base de datos, puerto local
-  user: 'akira',
-  password:'admin123',
-  database:'my_store' // nombre d ela base de datos
-});
+  // host:'localhost',
+  // port:5432, //donde esta corriendo la base de datos, puerto local
+  // user: 'akira',
+  // password:'admin123',
+  // database:'my_store' // nombre d ela base de datos
+
+  //ya no es necesario pasarle los datos como arriba
+
+
+
 
 //ya no hay necesidad de crear una conexion, la primera que se genera, genera la conexion para el resto
 module.exports = pool;
